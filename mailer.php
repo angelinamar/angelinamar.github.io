9ad3d1b68fd57aa58f9baca182b348fb3475acce
Anhelina Markovych <angelinamarkovych@gmail.com>
2021-10-16 15:24:05 +0300

    Add files via upload

---
+++ mailer.php
@@ -0,0 +1,10 @@
+﻿<?
+if (isset ($_POST['messageFF'])) {
+  mail ("angelinamarkovych@gmail.com",
+        "заповнена контактна форма с ".$_SERVER['HTTP_REFERER'],
+        "Імя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nПовідомлення: ".$_POST['messageFF']);
+  echo ('<p style="color: green">Ваше повідомлення отримано, дякуємо!</p>');
+}
+?>
+<!-- вариант с кнопкой -->
+<input type="button" onclick="history.back();" value="Назад"/>
